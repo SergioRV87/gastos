52d6ff713da36f0abb79caab640ed982e8f81871
Sergio <s87@hotmail.com>
2019-12-11 21:12:03 +0100

    Correccion de ruta en webphp

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('nuevo_gasto_vista', function () {
     return view('nuevo_gasto_vista');  
 });
 //Route::get('/', 'usuario_controller@show');
+Route::post('administracion_vista', 'administracion_controller@show');
 Route::post('nuevo_gasto_apertura', 'nuevo_gasto_controller@show');
 Route::post('nuevo_gasto_guardado', 'nuevo_gasto_controller@nuevo_gasto_guardado');
 Route::post('cierra_sesion', 'usuario_controller@logoff');
